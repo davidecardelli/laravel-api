69bd8c4d6cd7d4b54084172e8a4a4d85789a639e
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-20 13:26:44 +0100

    api show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -37,7 +37,10 @@ class ProjectController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $project = Project::find($id);
+        if (!$project) return response(null, 404);
+
+        return response()->json($project);
     }
 
     /**
